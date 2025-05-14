652f41fedc5d61437b342259579dee20a81e86b7
“LucasRZamboni” <lucasrzamboni@hotmail.com>
2025-05-14 10:08:16 -0300

    Move arquivos para novas pastas e adiciona ex01.php

---
+++ Excercios/Ex Arrays Associativo/ex01.php
@@ -0,0 +1,11 @@
+<?php
+    $usuario = [
+        "nome" => "Renan",
+        "idade" => 32,
+        "profissao" => "Suporte de TI"
+    ];
+    
+    print_r($usuario);
+    echo "<br><br>";
+    var_dump($usuario);
+?>
