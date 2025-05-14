f9b8e763e1b01d480b9c8776b84f2dc20eb1806d
Renan Ribeiro da Silva <renan_n64@hotmail.com>
2025-05-14 16:20:19 -0300

    Fixação

--- Excercicios/Operadores Aritméticos/ex02.php
+++ Excercicios/Operadores Aritméticos/ex02.php
@@ -11,3 +11,33 @@ Crie um script em PHP que:
     - Se for entre 5.0 e 6.9, mostrar: ⚠️ "Aluno em recuperação."
     - Se for menor que 5.0, mostrar: ❌ "Aluno reprovado."
 */
+
+# Verifica se os parâmetros foram passados corretamente:
+if (isset($_GET["a"], $_GET["x"], $_GET["y"], $_GET["z"])){
+    # Recebe váriaveis via URL:
+    $aluno = $_GET["a"];
+    $n1 = (float) $_GET["x"];
+    $n2 = (float) $_GET["y"];
+    $n3 = (float) $_GET["z"];
+
+    # Calcula média:
+    $media = ($n1 + $n2 + $n3) / 3;
+
+    echo "<h2>As notas do aluno $aluno são $n1, $n2 e $n3</h2>";
+
+    echo "<ul>";
+    echo "<li>Média do aluno " . $aluno . " é igual a " . number_format($media, 2, ",", ".") . "</li>";
+    echo "</ul>";
+
+    if ($media >= 7){
+        echo "✅ Aluno aprovado!";
+    } 
+    elseif ($media < 7 && $media >= 5){
+        echo "⚠️ Aluno em recuperação.";
+    } else {
+        echo "❌ Aluno reprovado";
+    }
+} else {
+    echo "Parâmetros ausentes na URL!";
+}
+
